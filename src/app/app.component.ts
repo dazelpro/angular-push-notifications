f435dc9737980048351aa60fc8b341cf66ab69dc
unknown <dazelpro@gmail.com>
2021-09-23 19:46:56 +0700

    set permission subscribing to push notification

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,29 @@
 import { Component } from '@angular/core';
+import { SwPush } from '@angular/service-worker';
+
+const VAPID_PUBLIC = 'BA3Y4EMn38L71aOBPJgtL9PQSsFldcAZXURzYwmaMxDpbmfmpV7ps0B4CEHUmVGxWdW0iPfKSO2nRlXCTwFmiQA'
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular-push-notification';
+
+    title = 'angular-push-notification';
+
+    constructor(swPush: SwPush) {
+        if (swPush.isEnabled) {
+            swPush
+                .requestSubscription({
+                    serverPublicKey: VAPID_PUBLIC,
+                })
+                .then(subscription => {
+                    // send subscription to the server
+                    console.log('Hei ' + subscription)
+                })
+                .catch(console.error)
+        }
+    }
+
 }
